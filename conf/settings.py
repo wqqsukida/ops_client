4c777e826a5878d39b7e7f9bba50399f818e1ae3
wuyifei <wuyifei19881027@163.com>
2018-12-12 16:21:41 +0800

    20181212更新: 1.更新api为线上

--- conf/settings.py
+++ conf/settings.py
@@ -13,8 +13,8 @@ PLUGIN_ITEMS = {
 }
 
 API_TOKEN = "7d6766a6s5f76safas657889hj78kf90"
-API = "http://10.0.2.20/api/server/"
-TASK_API = "http://10.0.2.20/api/task/"
+API = "http://10.0.4.186/api/server/"
+TASK_API = "http://10.0.4.186/api/task/"
 # STASK_API = "http://10.0.2.20/api/stask/"
 # FILE_API = "http://10.0.2.20/api/file/"
 
@@ -28,6 +28,6 @@ NVME_TOOL_PATH = os.path.join(BASEDIR,'nvme_tool')
 # SSH_PORT = 22
 # SSH_PWD = "sdf"
 
-CLIENT_VERSION = "v1.0.4"
+CLIENT_VERSION = "v1.0.5"
 
 LOG_FILE_PATH = os.path.join(BASEDIR,'log','cmdb.log')
