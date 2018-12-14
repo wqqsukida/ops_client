8353cb7085486a1e16411e5a3b7b0f40effbaf54
wuyifei <wuyifei19881027@163.com>
2018-12-14 14:43:54 +0800

    20181214更新: 1.update ---> v1.1.2 2.run_client.sh加入enable和disable方法

--- conf/settings.py
+++ conf/settings.py
@@ -28,6 +28,6 @@ NVME_TOOL_PATH = os.path.join(BASEDIR,'nvme_tool')
 # SSH_PORT = 22
 # SSH_PWD = "sdf"
 
-CLIENT_VERSION = "v1.1.1"
+CLIENT_VERSION = "v1.1.2"
 
 LOG_FILE_PATH = os.path.join(BASEDIR,'log')
