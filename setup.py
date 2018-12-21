7ec23117defb13172acf0a12423c188d1820b5e3
wuyifei <wuyifei19881027@163.com>
2018-12-21 15:47:28 +0800

    20181214更新: 1.update ---> v0.1.1 2.setup.py添加远程执行sudo设置

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ def setup():
         step3 = os.system("sed -i '$a\sh /opt/dera_ops_client/run_client.sh start' /etc/rc.d/rc.local & chmod +x /etc/rc.d/rc.local")
         if step3 != 0:
             print('\033[1;31;40m 设置开机启动失败,请手动将启动脚本添加到rc.local下!\033[0m')
+    step4 = os.system("sed -i 's/Defaults    requiretty/Defaults    !requiretty/g' /etc/sudoers")
 
     print('\033[1;32;40m 安装完成!\033[0m')
 
