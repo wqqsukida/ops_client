c1f016b30130a01d8614b72df91af282c8129e23
wuyifei <wuyifei19881027@163.com>
2019-03-29 09:37:05 +0800

    20190328更新: 1.更改server上報ip地址

--- conf/settings.py
+++ conf/settings.py
@@ -13,9 +13,9 @@ PLUGIN_ITEMS = {
 }
 
 API_TOKEN = "7d6766a6s5f76safas657889hj78kf90"
-API = "http://10.0.2.20/api/server/"
-TASK_API = "http://10.0.2.20/api/task/"
-UTASK_API = "http://10.0.2.20/api/utask/"
+API = "http://10.0.7.69/api/server/"
+TASK_API = "http://10.0.7.69/api/task/"
+UTASK_API = "http://10.0.7.69/api/utask/"
 
 TEST = False
 
